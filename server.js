a3d20eceac41124d44e488a5d7140c0602bacf44
ujvaljobs <111234610+ujvaljobs@users.noreply.github.com>
2022-08-14 21:23:21 +0530

    Allow all origin cors error solved

--- server.js
+++ server.js
@@ -4,28 +4,10 @@ const port = 3001;
 const app = express();
 const cors = require('cors')
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+	origin: "https://ujvaljob.com"
+}))
 
-function setHeaderFunc (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://ujvaljob.com');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-}
-
-app.use(setHeaderFunc);
 
 app.post('/order', (req, res) => {
 	const authHeader = `Basic ${btoa('rzp_test_IMzw5tP8UzxP6Q:auOjL7nS14GfjPEEGCjZYmRf')}`;
