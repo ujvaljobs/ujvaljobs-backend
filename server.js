1af03cff730771ce733bf66d3766d6932b184623
mrudulkolambe <mrudulkolambe02@gmail.com>
2022-08-14 19:37:23 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const { default: axios } = require('axios');
+const express = require('express');
+const port = 3001;
+const app = express();
+const cors = require('cors')
+app.use(express.json())
+app.use(cors())
+
+
+app.post('/order', (req, res) => {
+	const authHeader = `Basic ${btoa('rzp_test_IMzw5tP8UzxP6Q:auOjL7nS14GfjPEEGCjZYmRf')}`;
+	axios({
+		method: 'post',
+		url: `https://api.razorpay.com/v1/orders`,
+		headers: { "content-type": "application/json", 'authorization': authHeader },
+		data: {
+			amount: Number(req.query.amount),
+			currency: req.query.currency,
+		}
+	})
+		.then((response) => {
+			res.send(response.data)
+		})
+		.catch((err) => {
+			res.send(err)
+		})
+})
+
+
+app.listen(port, () => {
+	console.log(`Server Started on port ${port}`);
+})
